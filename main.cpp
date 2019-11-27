156d4553c9d4c839674a9c1a23d65206dcb66b16
Krunk-Juice <krunk_jc@yahoo.com>
2019-11-26 19:39:54 -0800

    minor clean up of execess libraries

--- main.cpp
+++ main.cpp
@@ -1,15 +1,6 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <vector>
-// #include "process.h"
-// #include "frame.h"
 #include "memory_manager.h"
 
 int main() {
-    // std::vector<Process> InputQueue;
-    // std::vector<Process> Processes;
-    // std::vector<Frame> MainMemory;
 
     Memory_Manager M;
 
